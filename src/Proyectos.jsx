e91ca3808c34b99ef82ac02cbfda4352b7e5a68e
Marchelo <jfmarchelo29@hotmail.com>
2024-12-12 02:30:58 -0500

    tabla de proyectos responsive

--- src/Proyectos.jsx
+++ src/Proyectos.jsx
@@ -1,29 +1,32 @@
 import t3damoscurso_img from './imagenes/proyectos/T3DAMOSCURSO.png'
 import './styles/proyectos.css'
-function Proyectos(){
-    return(
-        <div>
-            <h1 className="mb-4 section-title">Proyectos</h1>
-            <table class="table table-striped table-hover shadow-lg rounded">
-      <thead class="table-dark">
-        <tr>
-          <th></th>
-          <th>Descripción</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><img src={t3damoscurso_img} className='imagen-proyecto'/></td>
-          <td><strong>T3DAMOSCURSO</strong><br/>Maquetación de página web educativa hecha con React.</td>
-          <td>
-            <a href="https://marchelohappyprogrammer.github.io/TE3DAMOSCURSO/" class="btn btn-primary btn-sm" target="_blank">Visitar</a>
-          </td>
-        </tr>
-        
-      </tbody>
-    </table>
-        </div>
-    )
+
+function Proyectos() {
+  return (
+    <div>
+      <h1 className="mb-4 section-title">Proyectos</h1>
+      <div className="table-responsive">
+        <table className="table table-hover rounded">
+          <thead className="table-dark">
+            <tr>
+              <th></th>
+              <th>Descripción</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><img src={t3damoscurso_img} className='imagen-proyecto' /></td>
+              <td><strong>T3DAMOSCURSO</strong><br />Maquetación de página web educativa hecha con React.</td>
+              <td>
+                <a href="https://marchelohappyprogrammer.github.io/TE3DAMOSCURSO/" className="btn btn-primary btn-sm" target="_blank" rel="noopener noreferrer">Visitar</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
+
 export default Proyectos;
